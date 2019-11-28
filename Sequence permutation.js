fd7d314eba9da9582390a8f865225e44aae5d7a8
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-11-28 10:45:51 +0700

    Update Sequence permutation.js

--- Sequence permutation.js
+++ Sequence permutation.js
@@ -40,3 +40,29 @@ function permute(permutation) {
   }
   return result;
 }
+function permuteUniq(permutation) {
+  var length = permutation.length,
+      result = [permutation.slice()],
+      c = new Array(length).fill(0),
+      i = 1, k, p,
+      check=[permutation.slice().join`|`]
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      if (!check.includes(permutation.slice().join`|`)){
+      check.push(permutation.slice().join`|`)
+      result.push(permutation.slice());
+      }
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
+}
